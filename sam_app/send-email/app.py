809b131c307a1a4a2bedb4abe08e05b9a69cc4e3
Szymon Pałka <szymon.palka02@gmail.com>
2023-03-13 13:36:14 +0100

    parsing data to send emails

--- sam_app/send-email/app.py
+++ sam_app/send-email/app.py
@@ -3,9 +3,21 @@ from botocore.exceptions import ClientError
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from mako.template import Template
+from itertools import groupby
 
 email_from = '' 
 email_pass = ''
+
+def lambda_handler(event, context):
+    new_tracks = json.loads(event['message'])
+    if new_tracks != []:
+        send_email('', new_tracks)
+    return {
+            "statusCode": 200,
+            "body": json.dumps({
+                "message": "Success!"
+        }),
+        }
 
 def get_secret(secret_name):
     region_name = "eu-central-1"
@@ -22,6 +34,20 @@ def get_secret(secret_name):
         raise e
     return json.loads(get_secret_value_response['SecretString'])
 
+def parse_to_send(data):
+    def key(k):
+        return k['email']
+    split = []
+
+    for l in data:
+        emails = l['emails']
+        del l['emails']
+        for email in emails:
+            split.append({'email': email, 'album': l['album']})
+
+    split = sorted(split, key=key)
+    return [{'email': key, 'albumlist': list(value)} for key, value in groupby(split, key=key)]
+
 def send_email(email_to, new_tracks):
     mail_template=Template(filename='mail_template.html',input_encoding='utf-8', output_encoding='utf-8', encoding_errors='replace')
     html = mail_template.render_unicode(x=new_tracks)
@@ -33,14 +59,3 @@ def send_email(email_to, new_tracks):
     with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
         server.login(email_from, email_pass)
         server.sendmail(email_from, email_to, email_message.as_string())
-
-def lambda_handler(event, context):
-    new_tracks = json.loads(event['message'])
-    if new_tracks != []:
-        send_email('', new_tracks)
-    return {
-            "statusCode": 200,
-            "body": json.dumps({
-                "message": "Success!"
-        }),
-        }
